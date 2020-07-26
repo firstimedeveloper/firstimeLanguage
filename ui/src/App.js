3450a54bdf3fb51ce912a760e92571f050389930
Jun Hyuk Han <hanjun.1998@gmail.com>
2020-07-26 16:35:13 +0900

    Now user will be told to enter video id upon login and the available subtitles will be shown after button is pressed.

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import { findByTestId } from '@testing-library/react';
 
 const useDataApi = (initialUrl = "") => {
-  const [data, setData] = useState({track: [], lines: []});
+  const [data, setData] = useState({track: [], lines: [], error: []});
   const [url, setUrl] = useState(initialUrl);
 
   const [isLoading, setIsLoading] = useState(false);
@@ -57,7 +57,7 @@ function Search(props) {
   const [{ data, isLoading, isError }, doFetch] = useDataApi();
 
   let defaultLang = ""
-  const langListView = data ? (
+  const langListView = data.track ? (
     data.track.map((line,idx) => {
       if (idx === 0) {
         defaultLang = line.langCode
@@ -83,9 +83,7 @@ function Search(props) {
   }
 
   return ( 
-    <form onSubmit={
-    handleSubmit
-    }> 
+    <form onSubmit={handleSubmit}> 
     <label htmlFor="id">Video ID </label>
     <input
       name="id"
@@ -94,12 +92,90 @@ function Search(props) {
       value={id}
       onChange={event => setId(event.target.value)}
     />
-    <label htmlFor="lang">Available subtitles: </label>
-    <select name="lang" id="lang" onChange={event => setLang(event.target.value)} data-placeholder="Choose a Language...">
-      {/* <option value="">None</option> */}
-      {(langListView.length === 0) ? 
-      <option>None</option> : langListView}
-    </select>
+    {!data.error &&
+    <>
+      <label htmlFor="lang">Available subtitles: </label>,
+      <select name="lang" id="lang" onChange={event => setLang(event.target.value)} data-placeholder="Choose a Language...">
+        {langListView}
+      </select>,
+      <label htmlFor="tlang">Translate to </label>,
+      <select name="tlang" id="tlang" onChange={event => setTlang(event.target.value)} data-placeholder="Choose a Language...">
+        <option value="">None</option>
+        <option value="AF">Afrikaans</option>
+        <option value="SQ">Albanian</option>
+        <option value="AR">Arabic</option>
+        <option value="HY">Armenian</option>
+        <option value="EU">Basque</option>
+        <option value="BN">Bengali</option>
+        <option value="BG">Bulgarian</option>
+        <option value="CA">Catalan</option>
+        <option value="KM">Cambodian</option>
+        <option value="ZH">Chinese (Mandarin)</option>
+        <option value="HR">Croatian</option>
+        <option value="CS">Czech</option>
+        <option value="DA">Danish</option>
+        <option value="NL">Dutch</option>
+        <option value="EN">English</option>
+        <option value="ET">Estonian</option>
+        <option value="FJ">Fiji</option>
+        <option value="FI">Finnish</option>
+        <option value="FR">French</option>
+        <option value="KA">Georgian</option>
+        <option value="DE">German</option>
+        <option value="EL">Greek</option>
+        <option value="GU">Gujarati</option>
+        <option value="HE">Hebrew</option>
+        <option value="HI">Hindi</option>
+        <option value="HU">Hungarian</option>
+        <option value="IS">Icelandic</option>
+        <option value="ID">Indonesian</option>
+        <option value="GA">Irish</option>
+        <option value="IT">Italian</option>
+        <option value="JA">Japanese</option>
+        <option value="JW">Javanese</option>
+        <option value="KO">Korean</option>
+        <option value="LA">Latin</option>
+        <option value="LV">Latvian</option>
+        <option value="LT">Lithuanian</option>
+        <option value="MK">Macedonian</option>
+        <option value="MS">Malay</option>
+        <option value="ML">Malayalam</option>
+        <option value="MT">Maltese</option>
+        <option value="MI">Maori</option>
+        <option value="MR">Marathi</option>
+        <option value="MN">Mongolian</option>
+        <option value="NE">Nepali</option>
+        <option value="NO">Norwegian</option>
+        <option value="FA">Persian</option>
+        <option value="PL">Polish</option>
+        <option value="PT">Portuguese</option>
+        <option value="PA">Punjabi</option>
+        <option value="QU">Quechua</option>
+        <option value="RO">Romanian</option>
+        <option value="RU">Russian</option>
+        <option value="SM">Samoan</option>
+        <option value="SR">Serbian</option>
+        <option value="SK">Slovak</option>
+        <option value="SL">Slovenian</option>
+        <option value="ES">Spanish</option>
+        <option value="SW">Swahili</option>
+        <option value="SV">Swedish </option>
+        <option value="TA">Tamil</option>
+        <option value="TT">Tatar</option>
+        <option value="TE">Telugu</option>
+        <option value="TH">Thai</option>
+        <option value="BO">Tibetan</option>
+        <option value="TO">Tonga</option>
+        <option value="TR">Turkish</option>
+        <option value="UK">Ukrainian</option>
+        <option value="UR">Urdu</option>
+        <option value="UZ">Uzbek</option>
+        <option value="VI">Vietnamese</option>
+        <option value="CY">Welsh</option>
+        <option value="XH">Xhosa</option>
+      </select>
+    </>
+    }
   <button type="submit">Search</button>
 </form>
 )
@@ -129,12 +205,19 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Fragment>
-        {search}
-        {isError ? <div>Something went wrong...</div> : transcriptView}
-        
+      <header className="section navbar">
+        <a className="active" href="/">Home</a>
+        <a href="#history">History</a>
+      </header>
+      <main className="section main">
+        <Fragment>
+          <div className="search">{search}</div>
+          <div className="content">{isError ? <div>Something went wrong...</div> : transcriptView}</div>
           
-      </Fragment>
+            
+        </Fragment>
+      </main>
+      <footer className="section footer">Footer</footer>
     </div>
   );
 }
@@ -145,104 +228,3 @@ export default App;
 
 //dL5oGKNlR6I
 //iXLXLCcGINw
-/*
-<form onSubmit={event => {
-      props.doFetch(`https://junhyukhan.herokuapp.com/new?id=${id}&lang=${lang}&tlang=${tlang}`);
-      event.preventDefault();
-  }}>
-    <label htmlFor="id">Video ID </label>
-    <input
-      name="id"
-      id="id"
-      type="text"
-      value={id}
-      onChange={event => setId(event.target.value)}
-    />
-    <label htmlFor="lang">Available subtitle language </label>
-    <input
-      name="lang"
-      id="lang"
-      type="text"
-      value={lang}
-      onChange={event => setLang(event.target.value)}
-    />
-    <label for="tlang">Translate to </label>
-    <select name="tlang" id="tlang" onChange={event => setTlang(event.target.value)} data-placeholder="Choose a Language...">
-      <option value="">None</option>
-      <option value="AF">Afrikaans</option>
-      <option value="SQ">Albanian</option>
-      <option value="AR">Arabic</option>
-      <option value="HY">Armenian</option>
-      <option value="EU">Basque</option>
-      <option value="BN">Bengali</option>
-      <option value="BG">Bulgarian</option>
-      <option value="CA">Catalan</option>
-      <option value="KM">Cambodian</option>
-      <option value="ZH">Chinese (Mandarin)</option>
-      <option value="HR">Croatian</option>
-      <option value="CS">Czech</option>
-      <option value="DA">Danish</option>
-      <option value="NL">Dutch</option>
-      <option value="EN">English</option>
-      <option value="ET">Estonian</option>
-      <option value="FJ">Fiji</option>
-      <option value="FI">Finnish</option>
-      <option value="FR">French</option>
-      <option value="KA">Georgian</option>
-      <option value="DE">German</option>
-      <option value="EL">Greek</option>
-      <option value="GU">Gujarati</option>
-      <option value="HE">Hebrew</option>
-      <option value="HI">Hindi</option>
-      <option value="HU">Hungarian</option>
-      <option value="IS">Icelandic</option>
-      <option value="ID">Indonesian</option>
-      <option value="GA">Irish</option>
-      <option value="IT">Italian</option>
-      <option value="JA">Japanese</option>
-      <option value="JW">Javanese</option>
-      <option value="KO">Korean</option>
-      <option value="LA">Latin</option>
-      <option value="LV">Latvian</option>
-      <option value="LT">Lithuanian</option>
-      <option value="MK">Macedonian</option>
-      <option value="MS">Malay</option>
-      <option value="ML">Malayalam</option>
-      <option value="MT">Maltese</option>
-      <option value="MI">Maori</option>
-      <option value="MR">Marathi</option>
-      <option value="MN">Mongolian</option>
-      <option value="NE">Nepali</option>
-      <option value="NO">Norwegian</option>
-      <option value="FA">Persian</option>
-      <option value="PL">Polish</option>
-      <option value="PT">Portuguese</option>
-      <option value="PA">Punjabi</option>
-      <option value="QU">Quechua</option>
-      <option value="RO">Romanian</option>
-      <option value="RU">Russian</option>
-      <option value="SM">Samoan</option>
-      <option value="SR">Serbian</option>
-      <option value="SK">Slovak</option>
-      <option value="SL">Slovenian</option>
-      <option value="ES">Spanish</option>
-      <option value="SW">Swahili</option>
-      <option value="SV">Swedish </option>
-      <option value="TA">Tamil</option>
-      <option value="TT">Tatar</option>
-      <option value="TE">Telugu</option>
-      <option value="TH">Thai</option>
-      <option value="BO">Tibetan</option>
-      <option value="TO">Tonga</option>
-      <option value="TR">Turkish</option>
-      <option value="UK">Ukrainian</option>
-      <option value="UR">Urdu</option>
-      <option value="UZ">Uzbek</option>
-      <option value="VI">Vietnamese</option>
-      <option value="CY">Welsh</option>
-      <option value="XH">Xhosa</option>
-    </select>
-    <button type="submit">Search</button>
-  </form>
-  );
-*/
